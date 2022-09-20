afba2d8bcd49cd21fc104e97f4d0a761e717f432
Dane Roelofs <dane.l.roelofs@gmail.com>
2022-09-19 19:23:36 -0700

    refactor: :recycle: minor changes to info text

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,13 +12,13 @@ export class About extends Component {
         <Heading text='About Me' align={headingAlign} />
         <div>
           <p className='info'>
-            Hi, I'm Dane! I'm a Full Stack Web Developer living in San Diego, California. I got into the world of programming because of my desire to solve unique and complicated problems. I'm focused on making a user's experience pleasant and efficient.
+            Hi, I'm Dane! I'm a Full Stack Developer living in San Diego, California. I got into the world of programming because of my desire to solve unique and complicated problems. I've done it all, designing and coding web components, building well structured APIs, creating and using automated testing enviornments, quality assurance, integrating CI/CD pipelines, handling deployment processes and more. I'm always looking for new opportunities to keep things fresh.
           </p>
           <p className='info'>
-            I have a deep appreciation for developer community and their passion to come together and help each other out. I strive to be a part of this community. I'm looking to work with motivated people to come up with innovative technology for the future. I'm always looking for a chance to increase my knowledge as well as an opportunity to display my current skills.
+            I have a deep appreciation for the developer community and our passion to come together and help each other out. I'm looking to work with motivated people to come up with innovative technology for the future. I'm always looking for a chance to increase my knowledge as well as an opportunity to display my current skill set. I enjoy technology so much I spend 7 days a week on a computer trying to learn something new.
           </p>
           <p className='info'>
-            With my free time I enjoy spending time at the beach, hiking, solving puzzles, playing video games and watching baseball. I also have a specialty for briefly trying out new hobbies.
+            In my free time I enjoy working on side projects, learning new programming languages as well as tools to make the job easier, playing video games, watching baseball, going hiking, going to the gym, helping friends and family with projects.
           </p>
         </div>
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,70 +1,97 @@
-import React, { Component } from 'react'
-import bitmoji from '../assets/bitmoji.png'
+import React, { Component } from "react";
+import bitmoji from "../assets/bitmoji.png";
 
-import '../styles/Home.scss'
+import "../styles/Home.scss";
 
-import About from '../pages/About'
-import Carousel from '../components/Carousel'
-import Heading from '../components/Heading'
-import Game from '../components/Game'
+import About from "../pages/About";
+import Carousel from "../components/Carousel";
+import Heading from "../components/Heading";
+import Game from "../components/Game";
 
 export class Home extends Component {
   render() {
     return (
       <div>
-        <div
-          className='header'
-        >
-          <About
-            headingAlign='left'
-          />
+        <div className="header">
+          <About headingAlign="left" />
           <img
             src={bitmoji}
-            className='bitmoji'
-            alt='cat sleeping on laptop, preventing me to work'
+            className="bitmoji"
+            alt="cat sleeping on laptop, preventing me to work"
           />
         </div>
         <Carousel />
-        <div
-          id='game-info-container'
-        >
+        <div id="game-info-container">
           <Game />
-          <div
-            id='portfolio-info'
-          >
-            <Heading
-              align='left'
-              text='Collectible Game'
-            />
-            <p
-              className='info'
-            >
-              This is just a simple collectible game on a canvas element. It starts by generating a random position for the player and the collectible. While generating a new collectible location it will check that it is not within collectible range of the player. If it is, it will keep generating a new postion until it is not.
+          <div id="portfolio-info">
+            <Heading align="left" text="Portfolio Information" />
+            <p className="info">
+              I'm proud to announce that this entire portfolio was coded by hand
+              from scratch using React and Sass. The only libraries that are
+              used here are{" "}
+              <a
+                href="https://particles.js.org/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                tsParticles
+              </a>{" "}
+              and{" "}
+              <a
+                href="https://react-icons.github.io/react-icons/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                React Icons
+              </a>
+              . tsParticles was used for the background component and all the
+              icons come from React Icons. Big thanks to Quinn Roelofs (my
+              brother) for designing the logo that was used in the navigation
+              bar and web browser tab. Alien png image used in the collectible
+              game{" "}
+              <a
+                href="https://opengameart.org/content/alien-8"
+                target="_blank"
+                rel="noreferrer"
+              >
+                here
+              </a>
+              . Spaceship png image used in the collectible game{" "}
+              <a
+                href="https://opengameart.org/content/space-ship-0"
+                target="_blank"
+                rel="noreferrer"
+              >
+                here
+              </a>
+              . Check out the source code for this portfolio{" "}
+              <a
+                href="https://github.com/Rethora/portfolio"
+                target="_blank"
+                rel="noreferrer"
+              >
+                here
+              </a>
+              .
             </p>
-            <p
-              className='info'
-            >
-              Once the player and collectible postions are valid, it draws the canvas, and the images for the player and the collectible at their inital locations. Then it starts listening for keydown events. It checks that a player's next move is in range of the board and draws the new player's location on the next frame.
-            </p>
-            <p
-              className='info'
-            >
-              Once a player is in collectible range, it updates their score and generates a new postion for the next collectible. Once a player has collected five items, it draws the winning message on the screen and cancels the frame animations.
+            <p className="info">
+              I'm a huge fan of making changes to past projects where and when
+              they're needed. If you find an issue with this site or have a
+              suggestion for a new feature{" "}
+              <a
+                href="https://github.com/Rethora/portfolio/issues/new"
+                target="_blank"
+                rel="noreferrer"
+              >
+                open up a request
+              </a>
+              .
             </p>
           </div>
         </div>
-        <Heading
-          align='left'
-          text='Portfolio Information'
-        />
-        <p
-          className='info'
-        >
-          I'm proud to announce that this entire portfolio was coded by hand from scratch using React and Sass. The only libraries that are used here are <a href='https://particles.js.org/' target='_blank' rel='noreferrer'>tsParticles</a> and <a href='https://react-icons.github.io/react-icons/' target='_blank' rel='noreferrer'>React Icons</a>. tsParticles was used for the background component and all the icons come from React Icons. Big thanks to Quinn Roelofs (my brother) for designing the logo that was used in the navigation bar and web browser tab. Alien png image used in the collectible game <a href='https://opengameart.org/content/alien-8' target='_blank' rel='noreferrer'>here</a>. Spaceship png image used in the collectible game <a href='https://opengameart.org/content/space-ship-0' target='_blank' rel='noreferrer'>here</a>. Check out the source code for this portfolio <a href='https://github.com/Rethora/portfolio' target='_blank' rel='noreferrer'>here</a>.
-        </p>
       </div>
-    )
+    );
   }
 }
 
-export default Home
+export default Home;
